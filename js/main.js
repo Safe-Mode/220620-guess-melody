b29cca57165d1ca8cc14fd0d9caae379749b6d13
Fuckmaster <fuckmaster@yahoo.com>
2018-05-29 00:41:40 +0300

    Добавление переключения экранов с клавиатуры и мышью

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const Keycode = {
 
 const screenTemplates = document.querySelector(`#templates`).content
     .querySelectorAll(`.main`);
+const appEl = document.querySelector(`.app`);
 const mainEl = document.querySelector(`.main`);
 
 const showScreen = (number) => {
@@ -28,16 +29,65 @@ const renderScreen = () => {
   showScreen(screenNum);
 };
 
+const switchPrevScreen = () => {
+  if (screenNum > 0) {
+    screenNum--;
+    renderScreen();
+  }
+};
+
+const switchNextScreen = () => {
+  if (screenNum < screenTemplates.length - 1) {
+    screenNum++;
+    renderScreen();
+  }
+};
+
 let screenNum = 0;
 
 showScreen(screenNum);
 
 document.addEventListener(`keydown`, (evt) => {
-  if (screenNum > 0 && evt.keyCode === Keycode.Arrow.LEFT) {
-    screenNum--;
-    renderScreen();
-  } else if (screenNum < screenTemplates.length - 1 && evt.keyCode === Keycode.Arrow.RIGHT) {
-    screenNum++;
-    renderScreen();
+  switch (evt.keyCode) {
+    case Keycode.Arrow.LEFT:
+      switchPrevScreen();
+      break;
+    case Keycode.Arrow.RIGHT:
+      switchNextScreen();
+  }
+});
+
+const arrowsWrapHTML = `
+    <style>
+      .arrows__wrap {
+        position: absolute;
+        top: 135px;
+        left: 50%;
+        margin-left: -56px;
+      }
+      .arrows__btn {
+        background: none;
+        border: 2px solid black;
+        padding: 5px 20px;
+      }
+    </style>
+    <button class="arrows__btn"><-</button>
+    <button class="arrows__btn">-></button>`;
+
+const arrowsWrapEl = document.createElement(`div`);
+
+arrowsWrapEl.classList.add(`arrows__wrap`);
+arrowsWrapEl.innerHTML = arrowsWrapHTML;
+appEl.appendChild(arrowsWrapEl);
+
+arrowsWrapEl.addEventListener(`click`, (evt) => {
+  evt.preventDefault();
+
+  switch (evt.target.textContent) {
+    case `<-`:
+      switchPrevScreen();
+      break;
+    case `->`:
+      switchNextScreen();
   }
 });
