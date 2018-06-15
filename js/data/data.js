fb2ace61363ecc7aad151dc26304673a68c94fa3
Safe_Mode <safe2mode@gmail.com>
2018-06-16 01:40:54 +0300

    Шаблонизирует блок состояния

---
+++ js/data/data.js
@@ -0,0 +1,8 @@
+export const initialState = {
+  question: 0,
+  notes: 3,
+  time: {
+    minutes: `05`,
+    seconds: `00`
+  }
+};
--- js/header.js
+++ js/header.js
@@ -1,6 +1,9 @@
 import getElementFromTemplate from './get-element-from-template.js';
+import {initialState} from './data/data.js';
 
-const headerTemplate =
+const NOTES_MAX_COUNT = 3;
+
+const headerTemplate = (state) =>
   `<svg xmlns="http://www.w3.org/2000/svg" class="timer" viewBox="0 0 780 780">
     <circle
       cx="390" cy="390" r="370"
@@ -12,13 +15,14 @@ const headerTemplate =
     <img class="play-again__img" src="/img/melody-logo-ginger.png" alt="logo" width="177" height="76">
   </a>
   <div class="timer-value" xmlns="http://www.w3.org/1999/xhtml">
-    <span class="timer-value-mins">05</span><!--
+    <span class="timer-value-mins">${state.time.minutes}</span><!--
     --><span class="timer-value-dots">:</span><!--
-    --><span class="timer-value-secs">00</span>
+    --><span class="timer-value-secs">${state.time.seconds}</span>
   </div>
   <div class="main-mistakes">
-    <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
-    <img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">
+    ${new Array(NOTES_MAX_COUNT - state.notes)
+        .fill(`<img class="main-mistake" src="img/wrong-answer.png" width="35" height="49">`)
+        .join(``)}
   </div>`;
 
-export default getElementFromTemplate(headerTemplate);
+export default getElementFromTemplate(headerTemplate(initialState));
